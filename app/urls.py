ddbe8a36a23079c0322363157e894422851fece1
RVelasco26 <rtvelasco01@gmail.com>
2021-05-21 12:26:40 +0800

    removed other url [about, thankyou]

--- app/urls.py
+++ app/urls.py
@@ -5,14 +5,12 @@ urlpatterns = [
     path('', views.home),
     path('home/', views.home),
     path('survey/', views.survey, name="survey"),
-    path('about/', views.about, name="about"),
     path('login/', views.login, name="login"),
     #path('logout/', views.logoutUser, name="logout"),
     path('register/', views.register, name="register"),
     path('sentimentChart/', views.sentimentPage, name="sentiment"), 
     path('likertChart/', views.likertPage, name="likert"),
     path('aspectChart/', views.aspectPage, name="aspect"), 
-    path('thankyou/', views.submitted, name="submitted"),
     #for admin
     path('dashboard/', views.dashboard, name="dashboard"),
 
