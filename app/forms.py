2ae87869670513895c0c4f5ca71039a4f30a1801
RVelasco26 <rtvelasco01@gmail.com>
2021-05-21 20:41:52 +0800

    removed likert form, opinion form, survey form, added LikertSurveyForm

--- app/forms.py
+++ app/forms.py
@@ -29,63 +29,102 @@ ANSWER_LISTS = (('Strongly Agree', 'Strongly Agree'),
 course_list = Course.objects.values_list('id','course_name', named=True)
 COURSES = [tuple([x.course_name, x.course_name]) for x in course_list]
 
-
-class LikertForm(forms.Form):
+class LikertSurveyForm(forms.Form):
     course_name = forms.CharField(max_length=70, widget=forms.Select(choices=COURSES, attrs={'class' : 'form-control'}))
+
+    #1
     a1 = forms.CharField(label=questions[0].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa1 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #2
     a2 = forms.CharField(label=questions[1].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa2 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #3
     a3 = forms.CharField(label=questions[2].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa3 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #4
     a4 = forms.CharField(label=questions[3].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa4 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #5
     a5 = forms.CharField(label=questions[4].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa5 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #6
     a6 = forms.CharField(label=questions[5].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa6 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #7
     a7 = forms.CharField(label=questions[6].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa7 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #8
     a8 = forms.CharField(label=questions[7].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa8 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #9
     a9 = forms.CharField(label=questions[8].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa9 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #10
     a10 = forms.CharField(label=questions[9].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa10 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #11
     a11 = forms.CharField(label=questions[10].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa11 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #12
     a12 = forms.CharField(label=questions[11].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa12 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #13
     a13 = forms.CharField(label=questions[12].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
-    a14 = forms.CharField(label=questions[13].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa13 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
 
-    i1 = forms.CharField(label=questions[14].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
-    i2 = forms.CharField(label=questions[15].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
-    i3 = forms.CharField(label=questions[16].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
-   
-    ac1 = forms.CharField(label=questions[17].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
-    ac2 = forms.CharField(label=questions[18].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
-    ac3 = forms.CharField(label=questions[19].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
-    ac4 = forms.CharField(label=questions[20].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    #14  question.13.question_description
+    a14 = forms.CharField(label=questions[14].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    a15 = forms.CharField(label=questions[15].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    a16 = forms.CharField(label=questions[16].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    a17 = forms.CharField(label=questions[17].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sa14 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-class SurveyForm(forms.Form):
-    course_name = forms.CharField(max_length=70, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa1 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa2 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa3 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa4 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa5 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa6 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa7 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa8 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa9 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa10 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa11 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa12 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa13 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sa14 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    #15 - 17
+    i1 = forms.CharField(label=questions[18].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    si1 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    i2 = forms.CharField(label=questions[19].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    si2 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    i3 = forms.CharField(label=questions[20].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    si3 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-    si1 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    si2 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    si3 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    
-    sac1 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sac2 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sac3 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    sac4 = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    #18 question.21.question_description
+    ac1 = forms.CharField(label=questions[22].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    ac2 = forms.CharField(label=questions[23].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    ac3 = forms.CharField(label=questions[24].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sac1 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-class OpinionForm(forms.Form):
-    e1 = forms.CharField(label=questions[21].question_description, max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    e2 = forms.CharField(label=questions[22].question_description, max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    e3 = forms.CharField(label=questions[23].question_description, max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    #19
+    ac4 = forms.CharField(label=questions[25].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sac2 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #20 question.26.question_description
+    ac5 = forms.CharField(label=questions[27].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    ac6 = forms.CharField(label=questions[28].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    ac7 = forms.CharField(label=questions[29].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sac3 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #21
+    ac8 = forms.CharField(label=questions[30].question_description, max_length=255, widget=forms.RadioSelect(choices=ANSWER_LISTS))
+    sac4 = forms.CharField(label="Comment", max_length=255, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
+    #opinion
+    e1 = forms.CharField(label=questions[31].question_description, max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    e2 = forms.CharField(label=questions[32].question_description, max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    e3 = forms.CharField(label=questions[33].question_description, max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+
 
 class RegistrationForm(forms.Form):
     name = forms.CharField(label="Name", max_length=55, widget=forms.TextInput(attrs={'class' : 'form-control'}))
